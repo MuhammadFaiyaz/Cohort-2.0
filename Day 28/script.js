b3da2365bf5a703d0c8b86f8590a465241933477
Faiyaz <mk3590653@gmail.com>
2026-02-07 15:41:08 +0600

    try to answer the simple basic question of the task given in day 28

---
+++ Day 28/script.js
@@ -0,0 +1,9 @@
+alert("Hi there")
+
+let js = "JS is the brain of web development as it allows users to intreract with websites, manipulate the content, givining the comment, review oppurtunities. It adds functionality, logics and dynamic power to the code or website"
+
+prompt('your name')
+
+// some examples where const is useful are in discount or offers, to store object that can be modified but not be replaced, ans some mathematics terms such as exponential and gravity.
+
+// when anybody want to use a value that contains no value in the particular time one should use null. but when anybody doesn't know what will be the value he should use the value undefined. 
